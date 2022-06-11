1d7b7f9cb5b310f5dbef3cffc713c6cd617008b6
bakercampbell <100162472+bakercampbell@users.noreply.github.com>
2022-06-10 22:12:24 -0400

    Revert "more"
    This reverts commit 2ef5b0aa39ffb04d22ee2809b19f9a3ca390a3ce.

--- HumanConnection/Assets/Scripts/PlayerController1.cs
+++ HumanConnection/Assets/Scripts/PlayerController1.cs
@@ -1,59 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 
-[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
 public class PlayerController1 : MonoBehaviour
 {
-    [SerializeField]
-    private float playerSpeed = 2.0f;
-    [SerializeField]
-    private float jumpHeight = 1.0f;
-    [SerializeField]
-    private float gravityValue = -9.81f;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float extraHeight;
+    [SerializeField] private LayerMask ground;
 
-    private CharacterController controller;
-    private PlayerInput playerInput;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
+    private PlayerControls playerControls;
+    private Rigidbody rb;
+    private CapsuleCollider col;
 
-    private InputAction moveAction;
-    private InputAction jumpAction;
-    private InputAction aimAction;
-    private InputAction shootAction;
+    private bool isGrounded;
+
+
+    private void Awake()
+    {
+        //Calls the PLayer Input
+        playerControls = new PlayerControls();
+
+        //Components attached to the player
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+
+
+    }
+
+    //called when the script is enabled (necessary for input system)
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    //Also required for input system to function
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
 
     private void Start()
     {
-        controller = GetComponent<CharacterController>();
-        playerInput = GetComponent<PlayerInput>();
-        moveAction = playerInput.actions["Move"];
-        jumpAction = playerInput.actions["Jump"];
-        aimAction = playerInput.actions["Aim"];
-        shootAction = playerInput.actions["Shoot"];
+        //When Jump is pressed, passes value to the Jump function
+        playerControls.Inside.Jump.performed += ctx => Jump(ctx.ReadValue<float>());
+
     }
 
-    void Update()
+    private void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
-        Vector2 input = moveAction.ReadValue<Vector2>();
-
-        Vector3 move = new Vector3(input.x, 0, 0);
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-
-
-        // Changes the height position of the player..
-        if (jumpAction.triggered && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("Floor"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+
+    private void Jump(float val)
+    {
+        //Checks if button was pressed and player was grounded
+        if (val == 1 && isGrounded is true)
+        {
+            //add upward force on rigidbody
+            rb.AddForce(new Vector3(0, jumpSpeed), ForceMode.Impulse);
+        }
+    }
+
+
 }
