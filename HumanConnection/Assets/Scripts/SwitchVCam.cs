b2dbcd8994a18bdf3dae79f125f29973c020761d
bakercampbell <100162472+bakercampbell@users.noreply.github.com>
2022-06-14 03:56:10 -0400

    Complete overhaul of controls(lights broke idk...)

---
+++ HumanConnection/Assets/Scripts/SwitchVCam.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Cinemachine;
+
+public class SwitchVCam : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInput playerInput;
+    [SerializeField]
+    private int priorityBoostAmount = 10;
+    [SerializeField]
+    private Canvas thirdPersonCanvas;
+
+    private CinemachineVirtualCamera virtualCamera;
+    private InputAction aimAction;
+
+    private void Awake()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        aimAction = playerInput.actions["Aim"];
+    }
+
+    private void OnEnable()
+    {
+        aimAction.performed += _ => StartAim();
+        aimAction.canceled += _ => CancelAim();
+    }
+
+    private void OnDisable()
+    {
+        aimAction.performed -= _ => StartAim();
+        aimAction.canceled -= _ => CancelAim();
+    }
+
+    private void StartAim()
+    {
+        virtualCamera.Priority += priorityBoostAmount;
+    }
+
+    private void CancelAim()
+    {
+        virtualCamera.Priority -= priorityBoostAmount;
+
+    }
+}
