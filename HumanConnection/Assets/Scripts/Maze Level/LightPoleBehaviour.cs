24eb81d57c624bda2e4cf729b47b474d8aa804d7
kbreslow-dev <kbreslow@gmail.com>
2022-06-18 20:57:09 -0400

    beginnings of a tutorial

--- HumanConnection/Assets/Scripts/Maze Level/LightPoleBehaviour.cs
+++ HumanConnection/Assets/Scripts/Maze Level/LightPoleBehaviour.cs
@@ -8,12 +8,15 @@ public class LightPoleBehaviour : MonoBehaviour, Interactable
     public event OnLightOffDelegate lightOffEvent;
     public delegate void OnLightOnDelegate();
     public event OnLightOnDelegate lightOnEvent;
+    public delegate void OnCamera();
+    public event OnCamera onCameraEvent;
     public Light lightGlobe;
     public bool isOn = true;
     [SerializeField]
     GameObject particles;
     [SerializeField]
     float particleTimer, particleTimerReset;
+    public bool isVisible;
 
 
     private void Update()
@@ -50,6 +53,12 @@ public class LightPoleBehaviour : MonoBehaviour, Interactable
     {
         if (collision.gameObject.tag == "Bullet")
             Interact();
+    }
+
+    private void OnBecameVisible()
+    {
+        isVisible = true;
+        onCameraEvent?.Invoke();
     }
 
 }
